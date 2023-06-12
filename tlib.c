b1bbfb64f4a89e3cb9b09073292541f9ab764dea
sahamrit <amritsahu.iitkgp@gmail.com>
2023-06-13 00:24:18 +0530

    Add new tensor implementation

--- tlib.c
+++ tlib.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <malloc.h>
+#include <assert.h>
+#include <inttypes.h>
 
 struct tlib_context tlib_init(const struct tlib_init_params params)
 {
@@ -15,15 +17,112 @@ struct tlib_context tlib_init(const struct tlib_init_params params)
 
     if (mem_buffer == NULL)
     {
-        fprintf(stderr, "Aligned memory allocation failed, size requested - %d bytes.\n", mem_size_aligned);
+        fprintf(stderr, "STDERR: %s: Aligned memory allocation failed, size requested - %d bytes.\n", __FILE__, mem_size_aligned);
+        assert(false);
         return;
     }
 
     struct tlib_context ctx = (struct tlib_context){
-        /*mem_size*/ mem_size_aligned,
-        /*mem_buffer*/ mem_buffer,
+        /*mem_size =*/mem_size_aligned,
+        /*mem_buffer =*/mem_buffer,
+        /*n_objects =*/0,
+        /*object_begin =*/NULL,
+        /*object_end =*/NULL,
     };
     return ctx;
+}
+
+struct tlib_tensor *tlib_new_tensor_impl(struct tlib_context *ctx,
+                                         enum tlib_types type,
+                                         int n_dims,
+                                         int64_t *ne,
+                                         void *data)
+{
+    struct tlib_object *const cur_obj = ctx->object_end;
+    const size_t curr_offset = cur_obj == NULL ? 0 : cur_obj->offset;
+    const size_t curr_size = cur_obj == NULL ? 0 : cur_obj->size;
+    size_t curr_end = curr_offset + curr_size;
+    // current end of the memory pool
+
+    // calculate memory requirement for the new tensor
+    // new tensors are always contiguous
+    size_t size_needed = TLIB_TYPE_SIZE[type];
+    for (int i = 0; i < n_dims; i++)
+    {
+        size_needed *= ne[i];
+    }
+
+    struct tlib_object *const new_obj = (struct tlib_object *)(ctx->mem_buffer + curr_end);
+    size_needed += TLIB_TENSOR_SIZE;
+
+    if (curr_end + size_needed + TLIB_OBJECT_SIZE > ctx->mem_size)
+    {
+        fprintf(stderr, "STDERR: %s: pool memory space exceeded (needed %zu, avaialable %zu)",
+                __FILE__, curr_end + size_needed + TLIB_OBJECT_SIZE, ctx->mem_size);
+        assert(false);
+        return;
+    }
+
+    *new_obj = (struct tlib_object){
+        /*offset =*/curr_end + TLIB_OBJECT_SIZE,
+        /*size =*/size_needed,
+        /*next =*/NULL,
+    };
+
+    if (ctx->object_begin == NULL)
+    {
+        ctx->object_begin = new_obj;
+    }
+    else
+    {
+        cur_obj->next = new_obj;
+    }
+
+    struct tlib_tensor *const new_tensor = (struct tlib_tensor *)(ctx->mem_buffer + new_obj->offset);
+
+    *new_tensor = (struct tlib_tensor){
+        /*ndims =*/n_dims,
+        /*n_elements =*/{1, 1, 1, 1},
+        /*n_bytes =*/{0, 0, 0, 0},
+        /*grad =*/NULL,
+        /*src0 =*/NULL,
+        /*src1 =*/NULL,
+        /*is_param =*/false,
+        /*data =*/NULL, // TODO
+        /*type =*/type,
+        /*backend =*/TLIB_BACKEND_CPU,
+        /*op =*/TLIB_OP_NONE,
+        /*name =*/{0},
+    };
+
+    for (int i = 0; i < n_dims; i++)
+    {
+        new_tensor->n_elements[i] = ne[i];
+    }
+
+    ctx->n_objects++;
+
+    new_tensor->n_bytes[n_dims - 1] = TLIB_TYPE_SIZE[type];
+    for (int i = n_dims - 2; i >= 0; i--)
+    {
+        new_tensor->n_bytes[i] = new_tensor->n_bytes[i + 1] * new_tensor->n_elements[i];
+    }
+
+    return new_tensor;
+}
+struct tlib_tensor *tlib_new_tensor_1d(struct tlib_context *ctx,
+                                       enum tlib_types type,
+                                       int64_t ne)
+{
+    return tlib_new_tensor(ctx, type, 1, &ne);
+}
+
+struct tlib_tensor *tlib_new_tensor(struct tlib_context *ctx,
+                                    enum tlib_types type,
+                                    int n_dims,
+                                    int64_t *ne)
+{
+    return tlib_new_tensor_impl(ctx, type, n_dims, ne, NULL);
 }
 
 int main()
@@ -38,4 +137,10 @@ int main()
     ctx = tlib_init(params);
 
     fprintf(stdout, "STDOUT: %s: Context initialised with mem size: %d bytes", __FILE__, ctx.mem_size);
+
+    struct tlib_tensor *x = tlib_new_tensor_1d(&ctx, TLIB_TYPE_F32, 10);
+
+    fprintf(stdout, "STDOUT: %s: Allocated tensor of shape (%" PRId64 ", %" PRId64 ", %" PRId64 ", %" PRId64 ") and stride (%zu, %zu, %zu, %zu)",
+            __FILE__, x->n_elements[0], x->n_elements[1], x->n_elements[2], x->n_elements[3],
+            x->n_bytes[0], x->n_bytes[1], x->n_bytes[2], x->n_bytes[3]);
 }
--- tlib.h
+++ tlib.h
@@ -8,8 +8,26 @@
 // properties
 //
 
-#define TLIB_MEM_ALIGN 4 // bytes
+#define TLIB_MEM_ALIGN 4      // bytes
+#define TLIB_MAX_TENSOR_DIM 4 // 1D-4D support
+#define TLIB_MAX_NAME 32      // 32 bytes string
+//
+// types and backends
+//
 
+enum tlib_types
+{
+    TLIB_TYPE_F32,
+    TLIB_TYPE_COUNT
+};
+
+enum tlib_backend
+{
+    TLIB_BACKEND_CPU,
+};
+static const size_t TLIB_TYPE_SIZE[TLIB_TYPE_COUNT] = {
+    [TLIB_TYPE_F32] = sizeof(float),
+};
 //
 // context and memory related structs
 //
@@ -18,8 +36,10 @@ struct tlib_init_params
 {
     // memory pool
     size_t mem_size; // in bytes
-    // pool is allocated all at once.
-    // objects in runtime use this pool, with no allocations in runtime.
+    /*
+    pool is allocated all at once.
+    objects in runtime use this pool, with no allocations in runtime.
+    */
 
     void *mem_buffer; // if NULL memory will be allocated internally
 };
@@ -28,14 +48,72 @@ struct tlib_context
 {
     size_t mem_size;
     void *mem_buffer;
+
+    int n_objects;                    // number of objects in memory pool
+    struct tlib_object *object_begin; // linked list of objects in memory pool
+    struct tlib_object *object_end;
 };
 
 struct tlib_object
 {
-    size_t offset;
-    size_t size;
+    size_t offset; // absolute offset in buffer. (assuming start of buffer to be 0)
+    size_t size;   // size of the tensor object + data
+
+    /*
+    memory buffer layout
+    |object|   |tensor object| |tensor data|   |object|
+             ^                               ^
+             |  <------ size (bytes) ------> |
+           offset (bytes)                   next
+    */
+
+    struct tlib_object *next; // creates a linked list helping in retrieval of tensor from pool
+};
+static const size_t TLIB_OBJECT_SIZE = sizeof(struct tlib_object);
+//
+// tensor operations
+//
+enum tlib_ops
+{
+    TLIB_OP_NONE,
+    TLIB_OP_ADD,
+    TLIB_OP_MUL,
 };
 
+struct tlib_tensor
+{
+    int n_dims;
+    int64_t n_elements[TLIB_MAX_TENSOR_DIM]; // n elements per dimension - aka size.
+    size_t n_bytes[TLIB_MAX_TENSOR_DIM];     // stride in bytes per dimension
+
+    struct tlib_tensor *grad;
+    struct tlib_tensor *src0;
+    struct tlib_tensor *src1;
+
+    bool is_param;
+
+    void *data;
+    enum tlib_types type;
+    enum tlib_backend backend;
+    enum tlib_ops op;
+
+    char name[TLIB_MAX_NAME];
+};
+static const size_t TLIB_TENSOR_SIZE = sizeof(struct tlib_tensor);
+struct tlib_tensor *tlib_new_tensor_1d(struct tlib_context *ctx,
+                                       enum tlib_types type,
+                                       int64_t ne);
+
+struct tlib_tensor *tlib_new_tensor(struct tlib_context *ctx,
+                                    enum tlib_types type,
+                                    int n_dims,
+                                    int64_t *ne);
+
+struct tlib_tensor *tlib_new_tensor_impl(struct tlib_context *ctx,
+                                         enum tlib_types type,
+                                         int n_dims,
+                                         int64_t *ne,
+                                         void *data);
 //
 // init and memory management functions
 //
